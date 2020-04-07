d18fd72e915241c0fbf37920a1c6668e60109214
Alpha9912 <matthieu.reveyron@orange.fr>
2020-04-07 17:07:07 +0200

    création de la base de données et début du formulaire d'inscription

--- src/Controller/FoodRatingController.php
+++ src/Controller/FoodRatingController.php
@@ -4,6 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use App\Entity\Inscription;
+use App\Repository\InscriptionRepository;
+use App\Form\InscriptionType;
 
 class FoodRatingController extends AbstractController
 {
@@ -14,4 +20,34 @@ class FoodRatingController extends AbstractController
     public function home() {
         return $this->render('food_rating/accueil.html.twig');
     }
+
+    /**
+     * @Route("/food_rating/inscription", name="inscription")
+     */
+
+    public function formInscription(Inscription $inscription = null, Request $request) {
+        
+        if(!$inscription) {
+            $inscription = new Inscription();
+        }
+
+        $manager = $this->getDoctrine()->getManager();
+        $form = $this->createForm(InscriptionType::class, $inscription);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if($form['mdp']->getData() == $form['mdp_confirmation']->getData()) {
+                $manager->persist($inscription);
+                $manager->flush();
+
+                return $this->redirectToRoute('food_rating');
+            }
+        }
+        return $this->render('food_rating/inscription.html.twig', [
+            'formInscription' => $form->createView()
+        ]);
+
+    }
 }
+
+?>
